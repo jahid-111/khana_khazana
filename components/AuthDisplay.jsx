119360dcc2fcc76774cb35a572ecff0f0a3e596f
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-09-30 15:06:21 +0300

    auth&&UserToggler

--- components/AuthDisplay.jsx
+++ components/AuthDisplay.jsx
@@ -1,18 +1,48 @@
 "use client";
+
 import useAuth from "@/hooks/useAuth";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { FaRegUserCircle } from "react-icons/fa";
 
 const AuthDisplay = () => {
   const { auth } = useAuth();
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <>
-      {auth && (
-        <li className="py-2">
-          {auth?.firstName} {auth?.lastName}
-        </li>
+    <div className="mx-3 relative flex justify-center items-center ">
+      {auth ? (
+        <>
+          <button className="md:hidden" onClick={() => setToggle(!toggle)}>
+            <FaRegUserCircle className=" hover:text-orange-500" />
+          </button>
+          {toggle && (
+            <div className="absolute top-5 right-0 mt-2 w-48 bg-white border rounded-md shadow-lg">
+              <ul className="py-2">
+                <li className="px-4 py-2 hover:bg-gray-100">
+                  {auth?.firstName} {auth?.lastName}
+                </li>
+                <li className="px-4 py-2 bg-red-500 text-white hover:bg-red-600">
+                  <a href="/">Logout</a>
+                </li>
+              </ul>
+            </div>
+          )}
+
+          <ul className="hidden md:block">
+            <li className="py-2 text-gray-500">
+              {auth?.firstName} {auth?.lastName}
+            </li>
+          </ul>
+        </>
+      ) : (
+        <ul>
+          <li className="py-2 bg-[#eb4a36] px-6 rounded-md text-white">
+            <Link href="/login">Login</Link>
+          </li>
+        </ul>
       )}
-    </>
+    </div>
   );
 };
 
--- components/NavSection.jsx
+++ components/NavSection.jsx
@@ -17,23 +17,23 @@ const NavSection = () => {
           />
         </Link>
 
-        <ul className="flex gap-4 text-sm text-gray-500">
-          <li className="py-2 active">
-            <Link href="/">Home</Link>
-          </li>
+        <div className=" flex justify-center items-center">
+          <ul className="flex gap-4 text-sm text-gray-500">
+            <li className="py-2 active">
+              <Link href="/">Home</Link>
+            </li>
 
-          <li className="py-2">
-            <Link href="">Recipe</Link>
-          </li>
+            <li className="py-2">
+              <Link href="">Recipe</Link>
+            </li>
 
-          <li className="py-2">
-            <Link href="/">About us</Link>
-          </li>
+            <li className="py-2">
+              <Link href="/">About us</Link>
+            </li>
+          </ul>
 
-          <li className="py-2 bg-[#eb4a36] px-6 rounded-md text-white content-center">
-            <Link href="/login">Login</Link>
-          </li>
-        </ul>
+          <AuthDisplay />
+        </div>
       </div>
     </nav>
   );
